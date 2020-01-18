78e586fb7235beb6b4cd1e7206a0e7d9ac6a8d73
matemolnar88 <matemolnar88@gmail.com>
2020-01-18 17:32:26 +0100

    Styling the paddle and play area

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// selcet the canvas element
+const cvs = document.getElementById("breakout");
+const ctx = cvs.getContext("2d");
+
+// add border to canvas
+cvs.style.border = "1px solid #0ff";
+
+// make the line a little bit more thick
+cvs.style.border = "1px solid #0ff";
+
+// game vars and const
+const PADDLE_WIDTH = 100;
+const PADDLE_MARGIN_BOTTOM = 50;
+const PADDLE_HEIGHT = 20;
+
+// create the paddle
+const paddle = {
+  x : cvs.width/2 - PADDLE_WIDTH/2,
+  y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+  width : PADDLE_WIDTH,
+  height : PADDLE_HEIGHT,
+  dx : 5
+}
+
+// draw paddle
+function drawPaddle(){
+  ctx.fillStyle = "#2e3548";
+  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+  ctx.strokeStyle = "#ffcd05";
+  ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+drawPaddle();
